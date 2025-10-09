8039f70de0cb255d294f8666683ec8fbe737cced
Tyler Williams <tyler@coolsoftware.dev>
2025-10-09 15:14:42 -0400

    refactor: remove unused methods, test definition name

--- android/src/main/java/expo/modules/coolpdf/CoolPdfModule.kt
+++ android/src/main/java/expo/modules/coolpdf/CoolPdfModule.kt
@@ -14,28 +14,6 @@ class CoolPdfModule : Module() {
     // The module will be accessible from `requireNativeModule('CoolPdf')` in JavaScript.
     Name("CoolPdf")
 
-    // Defines constant property on the module.
-    Constant("PI") {
-      Math.PI
-    }
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
     // Enables the module to be used as a native view. Definition components that are accepted as part of
     // the view definition: Prop, Events.
     View(CoolPdfView::class) {
--- android/src/test/java/expo/modules/coolpdf/CoolPdfModuleTest.kt
+++ android/src/test/java/expo/modules/coolpdf/CoolPdfModuleTest.kt
@@ -16,23 +16,6 @@ class CoolPdfModuleTest {
     val module = CoolPdfModule()
     val definition = module.definition()
 
-    // The module name should be "CoolPdf"
-    assertNotNull("Module definition should not be null", definition)
-  }
-
-  @Test
-  fun `hello function returns greeting`() {
-    val module = CoolPdfModule()
-
-    // Test the hello() function
-    // Note: Direct function testing requires accessing the module's functions
-    // For now, this demonstrates the test structure
-    assertTrue("Module should be instantiated", module is CoolPdfModule)
-  }
-
-  @Test
-  fun `PI constant should equal Math PI`() {
-    // Test that the PI constant is correctly defined
-    assertEquals("PI should equal Math.PI", Math.PI, Math.PI, 0.0)
+    assertTrue(definition.name == "CoolPdf")
   }
 }
